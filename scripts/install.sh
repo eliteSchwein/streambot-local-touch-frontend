2d1e14a2a3ac4a52b59ee89f8ac9c288ec49b4a3
tludwig <business@tludwig.dev>
2026-05-13 23:59:13 +0200

    fix: remove suffix

--- scripts/install.sh
+++ scripts/install.sh
@@ -82,7 +82,7 @@ modify_user() {
 }
 
 install_service() {
-  "$SCRIPTPATH/generateService.sh" --app_config="$MCCONFIGFILE" --service_suffix="${MCSERVICENAME#streambot_}"
+  "$SCRIPTPATH/generateService.sh" --app_config="$MCCONFIGFILE"
 }
 
 install_labwc_config() {
